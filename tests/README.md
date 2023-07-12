a1dae92ad8de01e2f22cc312c5277183b076f78d
mfrankii <moshe.frankipour@intel.com>
2023-07-12 22:58:51 +0300

    update tests

--- tests/README.md
+++ tests/README.md
@@ -1,7 +1,12 @@
 # Testing
 
 ### Commands
+
 ```
 pip install -r requirements.txt
 python -m pytest
 ```
+
+### Screenshot
+
+![screenshot](image.png)
--- tests/test_http.py
+++ tests/test_http.py
@@ -1,14 +1,26 @@
 import requests
+import json
 
-BASE_URL = 'http://localhost:8080/'
+BASE_URL = 'http://localhost:9000'
+
+
+def test_health():
+    response = requests.get(BASE_URL)
+    assert response.status_code == 200
+    assert response.text == "Welcome to our Spring-Boot Application!"
 
 
 def test_get_logs():
     response = requests.get(BASE_URL + "/logs")
+    logs = response.json()
+    expected_logs = json.dumps(logs, separators=(
+        ',', ':'))  # Converting the json to string
+
     assert response.status_code == 200
+    assert response.text == expected_logs
 
 
-def test_create_new_transaction():
-    data = {}
-    response = requests.post(BASE_URL + "/transaction", data)
-    assert response.status_code == 201
+def test_transaction():
+    response = requests.get(BASE_URL + "/transaction")
+    assert response.status_code == 200
+    assert response.text == "New transaction have been made!"
